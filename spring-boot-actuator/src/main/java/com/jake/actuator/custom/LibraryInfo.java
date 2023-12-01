fc51ef395045ca1fef87bb3fe38687dce61c9bb0
jake <gerion@gmail.com>
2023-12-01 17:04:13 +0900

    WebEndpoint 적용

--- spring-boot-actuator/src/main/java/com/jake/actuator/custom/LibraryInfo.java
+++ spring-boot-actuator/src/main/java/com/jake/actuator/custom/LibraryInfo.java
@@ -1,10 +1,16 @@
 package com.jake.actuator.custom;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 public class LibraryInfo {
 
     private String name;
     private String version;
+
+    public LibraryInfo(String name) {
+        this.name = name;
+    }
 }
--- spring-boot-actuator/src/main/java/com/jake/actuator/custom/LibraryInfoEndpoint.java
+++ spring-boot-actuator/src/main/java/com/jake/actuator/custom/LibraryInfoEndpoint.java
@@ -2,15 +2,23 @@ package com.jake.actuator.custom;
 
 import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
 import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
+import org.springframework.boot.actuate.endpoint.web.annotation.WebEndpoint;
+import org.springframework.lang.Nullable;
 
+import java.io.IOException;
 import java.util.List;
 
-@Endpoint(id = "libraryInfo")
+//@Endpoint(id = "libraryInfo")
+@WebEndpoint(id = "libraryInfo")
 public class LibraryInfoEndpoint {
 
+    /**
+     * Controller 의 GetMapping 과 유사
+     * 메소드 파라메터의 경우 쿼리스트링으로 전달
+     *
+     */
     @ReadOperation
-    public List<LibraryInfo> getLibraryInfos() {
-
+    public List<LibraryInfo> getLibraryInfos(@Nullable String name) throws IOException {
         LibraryInfo libraryInfo = new LibraryInfo();
         libraryInfo.setName("logback");
         libraryInfo.setVersion("1.0.0");
@@ -19,6 +27,12 @@ public class LibraryInfoEndpoint {
         libraryInfo2.setName("jackson");
         libraryInfo2.setVersion("2.0.0");
 
-        return List.of(libraryInfo, libraryInfo2);
+        List<LibraryInfo> libraryInfos = List.of(libraryInfo, libraryInfo2);
+
+        if(name != null) {
+            libraryInfos = libraryInfos.stream().filter(lib -> lib.getName().equals(name)).toList();
+        }
+
+        return libraryInfos;
     }
 }
