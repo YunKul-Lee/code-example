5760751aaddf4b34dcdc232d1f93bf45fa3b3890
jake <gerion@gmail.com>
2023-12-02 11:08:37 +0900

    add controller

--- spring-redis-om/src/main/java/com/jake/redisom/repository/PostRepository.java
+++ spring-redis-om/src/main/java/com/jake/redisom/repository/PostRepository.java
@@ -4,8 +4,16 @@ import com.jake.redisom.model.Post;
 import com.redis.om.spring.repository.RedisDocumentRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PostRepository extends RedisDocumentRepository<Post, String> {
 
+    List<Post> findByClapCountBetween(int minClap, int maxClap);
 
+    List<Post> findByAuthor_NameAndAuthor_LastName(String name, String lastName);
+
+    List<Post> searchByContent(String text);
+
+    List<Post> findByComments_Content(String content);
 }
