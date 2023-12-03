e64e01af551ab1ece7da533ba2eedc0e5102228c
jake <gerion@gmail.com>
2023-12-03 22:20:23 +0900

    add spring-redis-cache modules

---
+++ spring-redis-om/src/test/java/com/jake/redisom/SpringRedisOmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jake.redisom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRedisOmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
