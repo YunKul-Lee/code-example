c24eaa2970e8a0c02b6ef43c0379cd5f22c1b04f
jake <gerion@gmail.com>
2023-12-04 17:06:37 +0900

    jdbc-client 모듈 추가

---
+++ spring-jdbc-client/src/main/java/com/jake/jdbcclient/SpringJdbcClientApplication.java
@@ -0,0 +1,13 @@
+package com.jake.jdbcclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJdbcClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJdbcClientApplication.class, args);
+	}
+
+}
