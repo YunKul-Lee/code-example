318c6cb804d97a23d26ffe3fb4fe584d5bea02d9
jake <gerion@gmail.com>
2023-12-06 10:46:54 +0900

    CustomIndicator 테스트

--- spring-boot-actuator/src/main/java/com/jake/actuator/custom/MyCustomHealthIndicator.java
+++ spring-boot-actuator/src/main/java/com/jake/actuator/custom/MyCustomHealthIndicator.java
@@ -5,17 +5,26 @@ import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.stereotype.Component;
 
 @Component
-public class CustomHealthIndicator implements HealthIndicator {
+public class MyCustomHealthIndicator implements HealthIndicator {
     @Override
     public Health health() {
+
+        Health.Builder builder = getStatus() ? Health.up() : Health.down();
+
         // up(), down(), outOfService() 등 빌더에서 상태값을 정하고 detail 정보가 존재하는 경우 추가
         Health health =
-        Health.up()
+                builder
                 .withDetail("key1", "value1")
                 .withDetail("key2", "value2")
                 .build();
         ;
 
         return health;
+    }
+
+    private boolean getStatus() {
+        if ( System.currentTimeMillis() % 2 == 0 ) return true;
+
+        return false;
     }
 }
