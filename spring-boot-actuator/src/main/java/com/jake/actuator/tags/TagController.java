fe71388e539ea8f50cea6985645b7fcbd751c57a
jake <gerion@gmail.com>
2023-12-12 15:56:15 +0900

    @Counted 에 extraTags 추가하기

--- spring-boot-actuator/src/main/java/com/jake/actuator/tags/TagController.java
+++ spring-boot-actuator/src/main/java/com/jake/actuator/tags/TagController.java
@@ -1,5 +1,6 @@
 package com.jake.actuator.tags;
 
+import io.micrometer.core.annotation.Counted;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,18 +11,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class TagController {
 
-    private final MyQueueManagerWithTags queueManager;
+//    private final MyQueueManagerWithTags queueManager;
 
+    @Counted(value = "my.counted", extraTags = {"type", "push"})
     @GetMapping("/push")
     public String push() {
-        queueManager.push();
+//        queueManager.push();
 
         return "ok";
     }
 
+    @Counted(value = "my.counted", extraTags = {"type", "pop"})
     @GetMapping("/pop")
     public String pop() {
-        queueManager.pop();
+//        queueManager.pop();
 
         return "ok";
     }
