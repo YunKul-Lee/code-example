a8e030c6de143d5d474cc74559a00d4623a1e1ed
jake <gerion@gmail.com>
2023-12-13 10:47:22 +0900

    spring-redis-cluster init

---
+++ spring-redis-cluster/src/test/java/com/jake/rediscluster/SpringRedisClusterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jake.rediscluster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRedisClusterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
