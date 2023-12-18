16c9ad7128fcac818eccf7ba6b952f0f7b5e75f6
jake <gerion@gmail.com>
2023-12-18 14:35:59 +0900

    controller 추가

---
+++ spring-reactive/src/main/java/com/jake/reactive/controller/StudentController.java
@@ -0,0 +1,51 @@
+package com.jake.reactive.controller;
+
+import com.jake.reactive.model.Student;
+import com.jake.reactive.service.StudentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/students")
+public class StudentController {
+
+    private final StudentService studentService;
+
+    @PostMapping("/save")
+    public Mono<Student> saveStudent(@RequestBody Student student) {
+        return studentService.saveSingleStudent(student);
+    }
+
+    @PostMapping("/saveAll")
+    public Flux<Student> saveAll(@RequestBody Flux<Student> students) {
+        return studentService.saveAllStudents(students);
+    }
+
+    @GetMapping("/student/{id}")
+    public Mono<Student> fetchStudent(@PathVariable Long id) {
+        return studentService.findStudentById(id);
+    }
+
+    @GetMapping("/allStudents")
+    public Flux<Student> getAllStudents() {
+        return studentService.findAllStudents();
+    }
+
+    @PutMapping("/student/{id}")
+    public Mono<Student> updateStudent(@RequestBody Student student, @PathVariable Long id) {
+        return studentService.updateStudent(student, id);
+    }
+
+    @DeleteMapping("/student/{id}")
+    public String deleteStudent(@PathVariable Long id) {
+        return studentService.deleteById(id);
+    }
+
+    @DeleteMapping("/deleteAll")
+    public void deleteAllDatabase() {
+        studentService.deleteAll();
+    }
+}
