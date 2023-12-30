e172976ae1129e9bdc810283ed27aca0180f957b
jake <gerion@gmail.com>
2023-12-30 11:41:25 +0900

    spring-boot-security 예제모듈 추가

---
+++ spring-boot-docker-compose-support/src/main/java/com/jake/compose/SpringBootDockerComposeSupportApplication.java
@@ -0,0 +1,13 @@
+package com.jake.compose;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDockerComposeSupportApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDockerComposeSupportApplication.class, args);
+    }
+
+}
---
+++ spring-boot-docker-compose-support/src/test/java/com/jake/compose/SpringBootDockerComposeSupportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jake.compose;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDockerComposeSupportApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
