faf8c817216bf55ed0ac3cec76a214b74d3d1523
jake <gerion@gmail.com>
2024-01-04 17:30:23 +0900

    add spring-boot-auth module

---
+++ spring-boot-auth/src/test/java/com/jake/auth/SpringBootAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jake.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
