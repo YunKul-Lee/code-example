4272aa1ab612a1316abbf97ad791848b85253f7f
jake <gerion@gmail.com>
2024-01-05 10:22:11 +0900

    add state-pattern module

---
+++ state-pattern/src/main/java/com/jake/behavior/StatePatternApplication.java
@@ -0,0 +1,13 @@
+package com.jake.behavior;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatePatternApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StatePatternApplication.class, args);
+	}
+
+}
