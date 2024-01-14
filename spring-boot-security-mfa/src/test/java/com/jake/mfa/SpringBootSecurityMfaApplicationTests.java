4b76d25df52853835d1bf5b2e60417d029c11722
jake <gerion@gmail.com>
2024-01-14 16:06:25 +0900

    add mfa module

---
+++ spring-boot-security-mfa/src/test/java/com/jake/mfa/SpringBootSecurityMfaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jake.mfa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSecurityMfaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
