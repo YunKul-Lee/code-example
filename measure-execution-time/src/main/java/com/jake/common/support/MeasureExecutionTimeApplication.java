084488eebcfc0479e45ca13e306dbf5147e74d19
jake <gerion@gmail.com>
2024-01-17 20:14:39 +0900

    add measure-execution-time module

---
+++ measure-execution-time/src/main/java/com/jake/common/support/MeasureExecutionTimeApplication.java
@@ -0,0 +1,13 @@
+package com.jake.common.support;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeasureExecutionTimeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeasureExecutionTimeApplication.class, args);
+    }
+
+}
---
+++ measure-execution-time/src/test/java/com/jake/common/support/MeasureExecutionTimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jake.common.support;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeasureExecutionTimeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
