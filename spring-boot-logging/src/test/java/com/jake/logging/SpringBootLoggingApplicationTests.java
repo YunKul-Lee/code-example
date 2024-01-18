fc42e26ed4f92b78cf567a73e2cabba3e75872b3
jake <gerion@gmail.com>
2024-01-18 14:47:37 +0900

    add module

---
+++ spring-boot-logging/src/test/java/com/jake/logging/SpringBootLoggingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jake.logging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLoggingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
