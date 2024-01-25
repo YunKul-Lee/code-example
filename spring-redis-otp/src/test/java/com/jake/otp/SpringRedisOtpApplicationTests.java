bf24c45a8ee945659fb960fe53f318cc8dcee377
jake <gerion@gmail.com>
2024-01-25 10:49:16 +0900

    add spring-redis-otp

---
+++ spring-redis-otp/src/test/java/com/jake/otp/SpringRedisOtpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jake.otp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRedisOtpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
